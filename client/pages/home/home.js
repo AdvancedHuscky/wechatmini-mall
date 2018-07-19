f2e3b1e50cef21dfd134e190bb15c0a20ae4d605
William <menzongwei@126.com>
2018-07-19 17:10:56 +0800

    detail page html finished

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,4 +1,5 @@
 const qcloud = require('../../vendor/wafer2-client-sdk/index.js')
+const config = require('../../config.js')
 
 Page({
 
@@ -50,16 +51,24 @@ Page({
       title: '商品数据加载中……',
     })
     qcloud.request({
-      url: 'https://hi7tf7jb.qcloud.la/weapp/product',
+      url: config.service.productList,
       success: result => {
         wx.hideLoading()
-        this.setData({
-          productList: result.data.data
-        })
+        if(!result.data.code){
+          this.setData({
+            productList: result.data.data
+          })
+        }else{
+          wx.showToast({
+            title: '商品数据加载失败',
+          })
+        }
       },
       fail: result => {
         wx.hideLoading()
-        console.log('error!')
+        wx.showToast({
+          title: '商品数据加载失败',
+        })
       }
     })
   },
